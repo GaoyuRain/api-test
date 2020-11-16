9d1dcce5faf4bdeab9673c7892fee6e0c9371057
GaoYu <gaoyu_rain@163.com>
2020-11-16 23:41:16 +0800

    locust config

--- utils_fj/locust_utils.py
+++ utils_fj/locust_utils.py
@@ -32,9 +32,7 @@ class LocustUtils:
         if is_stop_done:
             l_cmd += STOP_CMD
         # 执行命令
-        print('startloucst')
-        print(l_cmd)
-        # subprocess.call(l_cmd, shell=True)
+        subprocess.call(l_cmd, shell=True)
 
     @staticmethod
     def __start_worker_thread(file_name):
