d766cbd0d50eed720a3cdb2b52ef8e8aa2076070
GAOyuRain <gaoyu_rain@163.com>
2020-12-29 10:39:42 +0800

    更新logutils

--- utils_fj/log_utils.py
+++ utils_fj/log_utils.py
@@ -12,11 +12,15 @@ import constant
 
 class LogUtils:
     @staticmethod
-    def print_response(r: Response, comment=None):
+    def print_response(r: Response, comment=None, isformart=False):
+
         if constant.hasLog:
             if comment is not None:
                 print(f'接口: {comment}')
             print(f'CODE: {r.status_code}')
             print(f'URL: {r.url}')
-            print(f'RESULT: {json.dumps(r.json(), ensure_ascii=False)}')
+            if isformart:
+                print(f'RESULT: {json.dumps(r.json(), ensure_ascii=False, sort_keys=True, indent=2)}')
+            else:
+                print(f'RESULT: {json.dumps(r.json(), ensure_ascii=False)}')
             print('-' * 100)
